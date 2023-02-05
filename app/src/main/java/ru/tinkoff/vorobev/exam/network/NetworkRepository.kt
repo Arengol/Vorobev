3a756313bbeb9288ffa72e2920f3197251907556
Vladimir <worobev2001@gmail.com>
2023-02-05 21:47:01 +0300

    Edit network repository

--- app/src/main/java/ru/tinkoff/vorobev/exam/network/NetworkRepository.kt
+++ app/src/main/java/ru/tinkoff/vorobev/exam/network/NetworkRepository.kt
@@ -4,6 +4,6 @@ import ru.tinkoff.vorobev.exam.data.Film
 import ru.tinkoff.vorobev.exam.data.FilmItem
 
 interface NetworkRepository {
-    suspend fun getFilmItems (page: Int): ResultWrapper<List<FilmItem>>
+    suspend fun getFilmItems (): ResultWrapper<List<FilmItem>>
     suspend fun getFilmInfoById (id: Int): ResultWrapper<Film>
 }
--- app/src/main/java/ru/tinkoff/vorobev/exam/network/NetworkRepositoryImpl.kt
+++ app/src/main/java/ru/tinkoff/vorobev/exam/network/NetworkRepositoryImpl.kt
@@ -1,18 +1,21 @@
 package ru.tinkoff.vorobev.exam.network
 
+import android.annotation.SuppressLint
 import ru.tinkoff.vorobev.exam.data.Film
 import ru.tinkoff.vorobev.exam.data.FilmItem
 
 class NetworkRepositoryImpl (private val serverApi: ServerApi, private val apiKey: String): NetworkRepository {
-    override suspend fun getFilmItems(page: Int): ResultWrapper<List<FilmItem>> =
+    override suspend fun getFilmItems(): ResultWrapper<List<FilmItem>> {
+        val data = mutableListOf<FilmItem>()
         try {
-            ResultWrapper.Success(serverApi.getTopFilms(token = apiKey, page = page).transform())
-        }
-        catch (error: Throwable){
+            for (page in 1..20)
+            data.addAll(serverApi.getTopFilms(token = apiKey, page = page).transform())
+        } catch (error: Throwable) {
             println(error.message)
-            ResultWrapper.NetworkError
+        return    ResultWrapper.NetworkError
         }
-
+        return ResultWrapper.Success(data)
+    }
     override suspend fun getFilmInfoById(id: Int): ResultWrapper<Film> =
         try {
             ResultWrapper.Success(serverApi.getFilmInfo(token = apiKey, id = id).transform())
