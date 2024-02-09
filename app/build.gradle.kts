e01fabc0a5f20cbab593eea26afed813b4439bc2
Vladimir <worobev2001@gmail.com>
2024-02-09 21:43:23 +0300

    Add necessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id ("kotlin-parcelize")
+    id ("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "ru.tinkoff.vorobev.exam"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.tinkoff.vorobev.exam"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -44,4 +47,33 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation ("androidx.databinding:databinding-runtime:8.2.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0-alpha01")
+
+    kapt ("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation ("com.squareup.moshi:moshi-kotlin:1.13.0")
+    implementation ("com.squareup.moshi:moshi:1.13.0")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+
+    implementation ("com.github.bumptech.glide:glide:4.13.2")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+}
+
+kapt {
+    correctErrorTypes = true
 }
