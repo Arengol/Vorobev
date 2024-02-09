9ffd6dbfd200c67f550008481106aa5b007290c5
Vladimir <worobev2001@gmail.com>
2024-02-09 21:45:47 +0300

    Refactoring

--- app/src/main/java/ru/tinkoff/vorobev/exam/UI/MainActivity.kt
+++ app/src/main/java/ru/tinkoff/vorobev/exam/UI/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.tinkoff.vorobev.exam
+package ru.tinkoff.vorobev.exam.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.tinkoff.vorobev.exam.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
